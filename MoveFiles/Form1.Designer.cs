b22231f1eef7b4cc362d0f9406577048f0e35040
xujpvuxu <xujpvuxu2332@gmail.com>
2021-12-09 22:41:51 +0800

    Signed-off-by: xujpvuxu <xujpvuxu2332@gmail.com> 新增搬移程式

---
+++ MoveFiles/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace MoveFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Run = new System.Windows.Forms.Button();
+            this.SourcePath = new System.Windows.Forms.TextBox();
+            this.SelectSourcePath = new System.Windows.Forms.Button();
+            this.TargetPath = new System.Windows.Forms.TextBox();
+            this.SelectTargetPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(271, 319);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(99, 48);
+            this.Run.TabIndex = 1;
+            this.Run.Text = "搬移";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // SourcePath
+            // 
+            this.SourcePath.Location = new System.Drawing.Point(99, 96);
+            this.SourcePath.Name = "SourcePath";
+            this.SourcePath.Size = new System.Drawing.Size(515, 29);
+            this.SourcePath.TabIndex = 2;
+            // 
+            // SelectSourcePath
+            // 
+            this.SelectSourcePath.Location = new System.Drawing.Point(99, 144);
+            this.SelectSourcePath.Name = "SelectSourcePath";
+            this.SelectSourcePath.Size = new System.Drawing.Size(134, 31);
+            this.SelectSourcePath.TabIndex = 3;
+            this.SelectSourcePath.Text = "選擇來源目錄";
+            this.SelectSourcePath.UseVisualStyleBackColor = true;
+            this.SelectSourcePath.Click += new System.EventHandler(this.SelectSourcePath_Click);
+            // 
+            // TargetPath
+            // 
+            this.TargetPath.Location = new System.Drawing.Point(99, 206);
+            this.TargetPath.Name = "TargetPath";
+            this.TargetPath.Size = new System.Drawing.Size(515, 29);
+            this.TargetPath.TabIndex = 2;
+            // 
+            // SelectTargetPath
+            // 
+            this.SelectTargetPath.Location = new System.Drawing.Point(99, 250);
+            this.SelectTargetPath.Name = "SelectTargetPath";
+            this.SelectTargetPath.Size = new System.Drawing.Size(134, 31);
+            this.SelectTargetPath.TabIndex = 3;
+            this.SelectTargetPath.Text = "選擇目標目錄";
+            this.SelectTargetPath.UseVisualStyleBackColor = true;
+            this.SelectTargetPath.Click += new System.EventHandler(this.SelectTargetPath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SelectTargetPath);
+            this.Controls.Add(this.SelectSourcePath);
+            this.Controls.Add(this.TargetPath);
+            this.Controls.Add(this.SourcePath);
+            this.Controls.Add(this.Run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.TextBox SourcePath;
+        private System.Windows.Forms.Button SelectSourcePath;
+        private System.Windows.Forms.TextBox TargetPath;
+        private System.Windows.Forms.Button SelectTargetPath;
+    }
+}
+
---
+++ MoveFiles/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoveFiles
+{
+    /// <summary>
+    /// 搬移檔案
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 重建子
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 開始搬檔
+        /// </summary>
+        /// <param name="sender">搬檔按鍵</param>
+        /// <param name="e">搬檔事件</param>
+        private void Run_Click(object sender, EventArgs e)
+        {
+            (bool isEfficient, string path)[] pathData = new (bool efficient, string path)[2];
+            pathData[0] = GetPath(SourcePath.Text);
+            pathData[1] = GetPath(TargetPath.Text);
+
+            //檢查路徑
+            if (pathData.All(data => data.isEfficient))
+            {
+                Move(pathData[0].path, pathData[1].path);
+                MessageBox.Show("完成");
+            }
+        }
+
+        /// <summary>
+        /// 路徑是否有效
+        /// </summary>
+        /// <param name="path">路徑</param>
+        /// <returns>(是否有效,路徑)</returns>
+        private (bool isEfficient, string path) GetPath(string path)
+        {
+            return (Directory.Exists(path), path);
+        }
+
+        /// <summary>
+        /// 搬檔
+        /// </summary>
+        /// <param name="sourcePath">來源路徑</param>
+        /// <param name="targetPath">目標路徑</param>
+        private void Move(string sourcePath, string targetPath)
+        {
+            //目標路徑是否有資料夾
+            if (!Directory.Exists(targetPath))
+            {
+                Directory.CreateDirectory(targetPath);
+            }
+
+            //搬檔案
+            foreach (string perData in Directory.GetFiles(sourcePath))
+            {
+                File.Move(perData, Path.Combine(targetPath, Path.Combine(targetPath, Path.GetFileName(perData))));
+            }
+
+            //進入資料夾
+            foreach (string perPath in Directory.GetDirectories(sourcePath))
+            {
+                string fileName = Path.GetFileName(perPath);
+                Move(Path.GetFullPath(perPath), Path.Combine(targetPath, fileName));
+                Directory.Delete(perPath);
+            }
+        }
+
+        /// <summary>
+        /// 選擇來源路徑
+        /// </summary>
+        /// <param name="sender">來源路徑按鍵</param>
+        /// <param name="e">來源路徑事件</param>
+        private void SelectSourcePath_Click(object sender, EventArgs e)
+        {
+            SourcePath.Text = GetFilePath();
+        }
+
+        /// <summary>
+        /// 選擇目標路徑
+        /// </summary>
+        /// <param name="sender">來源路徑按鍵</param>
+        /// <param name="e">來源路徑事件</param>
+        private void SelectTargetPath_Click(object sender, EventArgs e)
+        {
+            TargetPath.Text = GetFilePath();
+        }
+
+        /// <summary>
+        /// 選擇的路徑
+        /// </summary>
+        /// <returns>輸出選擇的路徑</returns>
+        private string GetFilePath()
+        {
+            string path = string.Empty;
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "請選擇Txt所在資料夾";
+            if (dialog.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(dialog.SelectedPath))
+            {
+                path = dialog.SelectedPath;
+            }
+
+            return path;
+        }
+    }
+}
